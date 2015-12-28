408797e7f84e9a4b288c9edb964005bd8fa5a52a
ykptke <ykpteke@gmail.com>
2015-12-28 16:00:54 +0200

    Maps Container component'i yazılacak. fixed #5

--- app/packages/octopus-main/components/Map.jsx
+++ app/packages/octopus-main/components/Map.jsx
@@ -1,33 +1,42 @@
 /* global Meteor GoogleMaps */
-
-Meteor.startup(() => {
-  GoogleMaps.load();
-
-  Meteor.autorun(() => {
-    if (GoogleMaps.loaded()) {
-      GoogleMaps.create({
-	name: 'myMap',
-	element: document.getElementById('map'),
-	options: {
-	  center: new google.maps.LatLng(-37.8136, 144.9631),
-	  zoom: 8
-	}
-      });
-
-      GoogleMaps.ready('myMap', function(map) {
-	var marker = new google.maps.Marker({
-	  position: new google.maps.LatLng(-37.8136, 144.9631),
-	  map: map.instance
-	});
-      });
-    }
-  });
+MapContainer = React.createClass({
+  mixins: [ReactMeteorData],
+  componentDidMount() {
+    GoogleMaps.load();
+  },
+  getMeteorData() {
+    return {
+      loaded: GoogleMaps.loaded(),
+      mapOptions: GoogleMaps.loaded() && this._mapOptions()
+    };
+  },
+  _mapOptions() {
+    return {
+      center: new google.maps.LatLng(39.934486,32.853241),
+      zoom: 7
+    };
+  },
+  render() {
+    if (this.data.loaded)
+      return <Map name="mymap" options={this.data.mapOptions} />
+    
+    return <div>Harita yükleniyor...</div>;
+  }
 });
 
 Map = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+    options: React.PropTypes.object.isRequired
+  },
+  componentDidMount() {
+    GoogleMaps.create({
+      name: this.props.name,
+      element: document.getElementById('map'),
+      options: this.props.options
+    });
+  },
   render() {
-    return (
-      <div id="map" className="googleMap">harita</div>
-    );
+    return <div id="map" className="googleMap"></div>;
   }
 });
