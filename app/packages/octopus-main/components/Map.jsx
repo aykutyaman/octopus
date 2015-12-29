f106e7ef2ff24739e8c0dd3bbc4cc52734f8781c
Aykut Yaman <yaman.ayk@gmail.com>
2015-12-29 17:31:10 +0200

    Fixes #7

--- app/packages/octopus-main/components/Map.jsx
+++ app/packages/octopus-main/components/Map.jsx
@@ -18,7 +18,7 @@ MapContainer = React.createClass({
   },
   render() {
     if (this.data.loaded) {
-      return <Map name="mymap" options={this.data.mapOptions} />;
+      return <Map name="mymap" options={this.data.mapOptions} />
     }
     return <div>Harita yükleniyor...</div>;
   }
@@ -35,16 +35,65 @@ Map = React.createClass({
       element: document.getElementById('map'),
       options: this.props.options
     });
-
-    GoogleMaps.ready(this.props.name, function(map) {
-      new google.maps.Marker({
-	position: new google.maps.LatLng(39.934486, 32.853241),
-	map: map.instance
-      });
-    });
-
   },
   render() {
-    return <div id="map" className="googleMap"></div>;
+    console.log('render MAP');
+    return (
+      <div>
+	<div id="map" className="googleMap"></div>
+
+	<MeteorData
+		subscribe = { () => {
+			     return Meteor.subscribe('Track.foo') }}
+		fetch = { () => {
+			 return {data: Tracks.findOne()}}}
+		render = { ({loading, data}) => {
+			  return <MapMarker name="mymap"  data={data} loading={loading} />
+			  }}
+				 />
+      </div>
+    )
+  }
+});
+
+MapMarker = React.createClass({
+  componentDidMount() {
+    console.log('mount');
+  },
+  getLatLng() {
+    const locationArr = this.props.data ? this.props.data.location.coordinates : [];
+    return locationArr.length ? _.object(['lat', 'lng'], locationArr) : "";
+  },
+  setMarkerPosition() {
+    const latLng = this.getLatLng();
+    GoogleMaps.ready(this.props.name, (map) => {
+      var marker;
+
+      if (!latLng) {
+	return;
+      }
+
+      if (!marker) {
+	console.log('new marker');
+	marker = new google.maps.Marker({
+	  position: new google.maps.LatLng(latLng.lat, latLng.lng),
+	  map: map.instance
+	});
+      } else {
+	console.log('update marker');
+	marker.setPosition(latLng);
+      }
+
+      // Center and zoom the map view onto the current position.
+      //map.instance.setCenter(marker.getPosition());
+      //map.instance.setZoom(15);
+    });
+  },
+  render() {
+    if (!this.props.loading) {
+      // we can render marker
+      this.setMarkerPosition();
+    }
+    return <div></div>;
   }
 });
--- app/packages/octopus-tracks/publications.js
+++ app/packages/octopus-tracks/publications.js
@@ -1,1 +1,3 @@
-
+Meteor.publish('Track.foo', function() {
+  return Tracks.find({}, {sort: {createdAt: -1}, limit: 1});
+});
