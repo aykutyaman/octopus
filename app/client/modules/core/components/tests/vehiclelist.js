f50eca91ce59966e84fb3f2ebe33e706972d80a1
Aykut Yaman <yaman.ayk@gmail.com>
2016-02-25 18:19:21 +0200

    Fixes #48

---
+++ app/client/modules/core/components/tests/vehiclelist.js
@@ -0,0 +1,29 @@
+import React from 'react';
+const {describe, it} = global;
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import VehicleList from '../vehiclelist.jsx';
+
+describe('core.components.vehiclelist', () => {
+  const vehicles = [
+    {name: 't-one', _id: 'one'},
+    {name: 't-two', _id: 'two'},
+  ];
+
+  it('handler must have a default click callback', () => {
+    const el = shallow(<VehicleList vehicles={vehicles} />);
+    const list = el.find('a.list-group-item');
+    expect(list.first().prop('onClick')).to.be.a('function');
+  });
+
+  it('handler must call click callback', done => {
+    const handler = (e) => {
+      expect(e).to.be.equal(vehicles[0]['_id']);
+      done();
+    };
+    const el = shallow(<VehicleList vehicles={vehicles}  handler={handler} />);
+    const firstItem = el.find('a.list-group-item').first();
+    firstItem.simulate('click');
+  });
+
+});
--- app/client/modules/core/components/vehiclelist.jsx
+++ app/client/modules/core/components/vehiclelist.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const VehicleList = ({vehicles, handler}) => (
+const VehicleList = ({vehicles = [], handler = () => null }) => (
   <div className="panel panel-default">
-    <div className="panel-heading">Araçlar {handler}  </div>
+    <div className="panel-heading">Araçlar</div>
     <div className="list-group">
       {vehicles.map(vehicle => (
 	<a href='#' className="list-group-item" onClick={() => handler(vehicle._id)} key={vehicle._id} value={vehicle._id}>{vehicle.plate}</a>
